f0d298a4cf4e279aed3b495367cf33ec18ad77c1
riyanshi-ji <riyanshijain370@gmail.com>
2026-01-30 13:14:10 +0530

    Lab_01_Sem-04

---
+++ index.js
@@ -0,0 +1,25 @@
+/*console.log("hello world")
+console.error("zero division error");
+console.warn("");
+
+
+let students={ name:"riyanshi", age:28, course="cse"};
+console.table(students);*/
+
+
+/*console.time("test");
+for(let i=0;i<10000;i++){}
+
+console.timeEnd("test");*/
+
+/*console.group("cse24")
+console.log("name:riyanshi");
+
+console.groupEnd{};*/
+
+/*console.count("click");
+console.count("click");
+console.count("click");
+console.count("click");*/
+
+console.dir(document.body);

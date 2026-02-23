97d34575ac3e88942e5d200ab8abf14c15a993b9
riyanshi-ji <riyanshijain370@gmail.com>
2026-02-23 12:56:56 +0530

    Add files via upload

---
+++ longestprefix.js
@@ -0,0 +1,17 @@
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+    
+    strs.sort();
+    
+    let first = strs[0];
+    let last = strs[strs.length - 1];
+    let i = 0;
+    
+    while (i < first.length && first[i] === last[i]) {
+        i++;
+    }
+    
+    return first.substring(0, i);
+};
+let arr = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(arr));
